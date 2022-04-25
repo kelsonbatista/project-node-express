f0d24cd4c8e48ac26bc16fcbf05508189403c3e3
kelsonbatista <kelsonbatista@gmail.com>
2022-04-25 18:38:14 -0300

    Requisito 5

--- index.js
+++ index.js
@@ -9,15 +9,25 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const getTalkers = async () => {
-  const talkers = await fs.readFile('./talker.json', 'utf-8')
+const getTalkers = () => {
+  const talkers = fs.readFile('./talker.json', 'utf-8')
     .then((content) => JSON.parse(content));
   return talkers;
 };
 
-const generateToken = () => {
-  return crypto.randomBytes(8).toString('hex');
+const setTalkers = (content) => {
+  const talkers = fs.writeFile('./talker.json', JSON.stringify(content));
+  return talkers;
 };
+
+const generateToken = () => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return token;
+};
+
+// const generateId = () => {
+//   const talkers = getTalkers();
+// };
 
 const validateLogin = (req, res, next) => {
   const { email, password } = req.body;
@@ -34,6 +44,58 @@ const validateLogin = (req, res, next) => {
   if (!validatePassword) {
     return res.status(400)
     .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+const validateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if ((token.toString()).length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+};
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400)
+    .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (!(Number.isInteger(age) && age >= 18)) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateDate = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const regexDate = /^\d{1,2}\/\d{1,2}\/\d{4}$/i;
+  if (!regexDate.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (!(Number.isInteger(rate) && rate >= 1 && rate <= 5)) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
   next();
 };
@@ -60,6 +122,24 @@ app.post('/login', validateLogin, (req, res) => {
   const token = generateToken();
   return res.status(200).json({ token });
 });
+
+app.post(
+  '/talker',
+  validateToken,
+  validateAge,
+  validateName,
+  validateTalk,
+  validateDate,
+  validateRate,
+  async (req, res) => {
+  const newTalker = req.body;
+  Object.assign(newTalker, { id: 5 });
+  const talkers = await getTalkers();
+  talkers.push(newTalker);
+  await setTalkers(talkers);
+  return res.status(201).json(newTalker);
+  },
+);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
