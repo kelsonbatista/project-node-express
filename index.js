da130c522bb6098a83d9d274e27bb52d2bda71dc
kelsonbatista <kelsonbatista@gmail.com>
2022-04-25 15:09:02 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8')
+  .then((response) => JSON.parse(response));
+  return res.status(200).json(talkers);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
