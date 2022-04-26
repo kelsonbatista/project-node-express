e0a3e80d071f84cb42716d85d342fde31c5b534f
kelsonbatista <kelsonbatista@gmail.com>
2022-04-26 14:18:47 -0300

    Fix: validadeTalk

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const validateRate = (req, res, next) => {
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
   if (!talk) {
-    return res.status(500)
+    return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
   if (talk.rate === 0) {
