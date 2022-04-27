4f79fd7559e0505a4a37ac92b491241997c70991
kelsonbatista <kelsonbatista@gmail.com>
2022-04-26 22:16:10 -0300

    Refatoração - middleware de erro

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -2,6 +2,7 @@ const sc = require('../utils/statusCode');
 
 const errors = (err, _req, res, _next) => {
   if (err) {
+    console.log(err.message);
     return res.status(sc.INTERNAL_SERVER_ERROR)
       .json({ message: 'Error 500 - Internal Server Error' });
   }
